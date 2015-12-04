c4031ac769a9b24e9d8c4bed21dc8af308e2ea0e
Ilya Sadykov <smecsia@gmail.com>
2015-12-04 16:18:29 +0300

    Eliminating camel injection for ping servlet

--- camelot-web/src/main/java/ru/yandex/qatools/camelot/web/PingResource.java
+++ camelot-web/src/main/java/ru/yandex/qatools/camelot/web/PingResource.java
@@ -1,12 +1,9 @@
 package ru.yandex.qatools.camelot.web;
 
-import org.apache.camel.Produce;
-import org.apache.camel.ProducerTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-import ru.yandex.qatools.camelot.Constants;
+import ru.yandex.qatools.camelot.api.EventProducer;
+import ru.yandex.qatools.camelot.api.annotations.MainInput;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -21,13 +18,14 @@ import static ru.yandex.qatools.camelot.api.Constants.Headers.UUID;
 @Scope("request")
 public class PingResource {
 
-    @Produce(uri = Constants.INPUT_QUEUE)
-    private ProducerTemplate testProducer;
+    @MainInput
+    EventProducer producer;
+
 
     @GET
     @Produces({MediaType.TEXT_PLAIN})
     public Response ping() {
-        testProducer.sendBodyAndHeader(null, UUID, "ping");
+        producer.produce(null, UUID, "ping");
         return Response.ok().build();
     }
 }
